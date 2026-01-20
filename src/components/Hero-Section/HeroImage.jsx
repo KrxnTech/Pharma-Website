4ff5da37753aae96b99ce4c3a5378e88b762e1b1
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-20 20:40:40 +0530

    Image change imported direct address from google !

--- src/components/Hero-Section/HeroImage.jsx
+++ src/components/Hero-Section/HeroImage.jsx
@@ -2,7 +2,7 @@ export default function HeroImage() {
     return (
         <div>
             <div className="Image-Div">
-                <img src="src\assets\WhatsApp Image 2026-01-20 at 8.03.27 PM (2).jpeg" alt="" />
+                <img src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSyYlwZc0P8jH6haNVwSerxyY_88uC4XkUnWyD0lGwtyhgcFVjB7EUiHrzjRpvHOUveCDCnQVJEVNX60jcbF88b3yON7_R7hhrsqj0TOAVgdZPA29BKjfGI4G9c5EL0b0UFrrUq-=s1360-w1360-h1020-rw" alt="" />
             </div>
         </div>
     )
