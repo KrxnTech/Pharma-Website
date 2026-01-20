0c10449e09f4587549ae7338bdb78cb072a304a2
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-20 17:03:06 +0530

    Gulkas Pharam Client Work ... Folder Setup Done | Linking | Installation's | Navbar complete with proper Jsx and CSS code !!

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css"
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="Nav-Container">
+                <div className="Logo-Container">
+                    <div><i class="fa-solid fa-flask"></i></div>
+                    <div className="Logo-Text"><b>Gulkas</b> <b className="Pharma-Text">Pharma</b></div>
+                </div>
+                <div className="Nav-Item-Link">
+                    <ul className="Nav-Ul">
+                        <li id="HOME">Home</li>
+                        <li>About</li>
+                        <li>Products</li>
+                        <li>Quality</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div className="Nav-Right">
+                    <div className="Theme-Icon"><i class="fa-regular fa-moon"></i></div>
+                    <div><button>Get Quote</button></div>
+                </div>
+            </div>
+        </nav>
+    )
+}
