74c835ae6eed75a9bcb624c7a30cb29b1b04acde
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-23 11:28:44 +0530

    Created AC page of about us main JSX and CSS , minor changes in History and MVV css

--- src/components/About-usMain/About-usMain-AC.jsx
+++ src/components/About-usMain/About-usMain-AC.jsx
@@ -1,30 +1,30 @@
 import "./About-usMain-AC.css"
 export default function CA() {
     return (
-        <div>
+        <div className="AC-main-div">
             <div className="Heading-Para-div">
                 <h1>Certifications & Accreditations</h1>
                 <p>Our commitment to quality is backed by internationally recognized certifications</p>
             </div>
-            <div className="4-Achivements">
+            <div className="Four-Achivements">
                 <div>
                     <i class="fa-solid fa-trophy"></i>
-                    <p>GMP Certified</p>
+                    <p><b>GMP Certified</b></p>
                     <p>The manufacturing facility operates strictly as per GMP standards with contamination-free closed production cycles.</p>
                 </div>
                 <div>
                     <i class="fa-solid fa-trophy"></i>
-                    <p>ISO 9001:2015</p>
+                    <p><b>ISO 9001:2015</b></p>
                     <p>Gulkas Pharma Pvt. Ltd. is ISO 9001:2015 certified ensuring a robust quality management system across operations.</p>
                 </div>
                 <div>
                     <i class="fa-solid fa-trophy"></i>
-                    <p>ISO 14001</p>
+                    <p><b>ISO 14001</b></p>
                     <p>The company follows structured environmental and EHS compliance practices aligned with international standards.</p>
                 </div>
                 <div>
                     <i class="fa-solid fa-trophy"></i>
-                    <p>WHO-GMP</p>
+                    <p><b>WHO-GMP</b></p>
                     <p>The facility is regularly audited and approved by multinational pharmaceutical companies for GMP compliance.</p>
                 </div>
             </div>
