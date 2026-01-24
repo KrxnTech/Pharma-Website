0bef840cca26a5f6ea34c392c568a8b33bdc8ac2
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-24 16:35:07 +0530

    Added Main Product JSX in APP JSX for Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 // SETUP ROUTES
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero-Section/Hero";
-import AboutUsMain from "./components/About-usMain/About-usMain";
+// import Navbar from "./components/Navbar/Navbar";
+// import Hero from "./components/Hero-Section/Hero";
+// import AboutUsMain from "./components/About-usMain/About-usMain";
+import MainProductBody from "./components/Product-Section/MainProductBody";
 
 
 export default function App() {
 
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/AboutUs" element={<AboutUsMain />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <MainProductBody />
     </>
   );
 } 
