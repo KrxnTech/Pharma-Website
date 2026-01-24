b4afd5154908b90b597cae58fb4893d7ac5a56d5
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-24 22:07:42 +0530

    Heading of Product Page .. Done

--- src/components/Product-Section/MainProductBody.jsx
+++ src/components/Product-Section/MainProductBody.jsx
@@ -1,7 +1,9 @@
 import ProductList from "./ProductList"
+import PSHeadingDesc from "./PSHeadingDesc"
 export default function MainProductBody() {
     return (
         <div>
+            <PSHeadingDesc />
             <ProductList />
         </div>
     )
---
+++ src/components/Product-Section/PSHeadingDesc.jsx
@@ -0,0 +1,9 @@
+import "./PSHeadingDesc.css"
+export default function PSHeadingDesc() {
+    return (
+        <div className="PSHeadingDesc_Div">
+            <h2>Our <b>Products</b></h2>
+            <p>Browse our comprehensive catalog of pharmaceutical chemicals intermediates and specialty compounds</p>
+        </div>
+    )
+}
