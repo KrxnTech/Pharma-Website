b86633d4cb82928a3f6dc2efd580109cfd8afa8c
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-25 15:36:48 +0530

    Change the Minor Padding of ProductCardList and Create a Filter Box for Real time Product Search Feature

--- src/components/Product-Section/ProductFilter.jsx
+++ src/components/Product-Section/ProductFilter.jsx
@@ -1,23 +1,27 @@
+import { useState } from "react"
 import "./ProductFilter.css"
 export default function FilterProduct() {
+    let [Input_value, setInputValue] = useState("")
+
+    let ShowValue = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    let ClearFilter = () => {
+        setInputValue("")
+    }
     return (
         <div className="Filter_Div">
-            <h5>Filter Box</h5>
-            <input type="text" />
-            <select name="" id="">
-                <option value="" disabled>Select Categories</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
-            <select name="" id="">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
+            <h5><i class="fa-solid fa-flask"></i> Filter Products</h5>
+            <input onChange={ShowValue} value={Input_value} type="text" placeholder="Search by name or CAS number" />
+            <div className="Filter_Button">
+                <div>
+                    <button>Filter</button>
+                </div>
+                <div>
+                    <button onClick={ClearFilter}>Clear</button>
+                </div>
+            </div>
         </div>
     )
 }
