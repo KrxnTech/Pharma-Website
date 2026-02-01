2c63c7834ad5085c9b7d44644ebd02ab8a4fbebc
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-02-01 09:54:48 +0530

    Added Routes Code for Contact Component Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero-Section/Hero";
 import AboutUsMain from "./components/About-usMain/About-usMain";
 import MainProductBody from "./components/Product-Section/MainProductBody";
 import MainPage from "./components/ViewProduct/MainPage";
+import ContactHeading from "./components/ContactSection/ContactHeading";
 
 export default function App() {
 
@@ -18,8 +19,9 @@ export default function App() {
           <Route path="/AboutUs" element={<AboutUsMain />} />
           <Route path="/Products" element={<MainProductBody />} />
           <Route path="/viewProduct/:CASNo" element={<MainPage />} />
+          <Route path="/Contact" element={<ContactHeading />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter >
     </>
   );
 } 
