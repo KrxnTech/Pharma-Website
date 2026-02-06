669882476429e7648a71752007942e85bf7c431c
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-02-06 22:09:57 +0530

    Preparing a good HTML Responce for USER ( Sending email as responce ) used nodemailer

--- Backend/routes/contact.routes.js
+++ Backend/routes/contact.routes.js
@@ -1,16 +1,46 @@
 import express from "express"
+import transporter from "../config/mailer.js"
 const router = express.Router()
 
-router.post("/send", (req, res) => {
-    const ForData = req.body
+router.post("/send", async (req, res) => {
+    const {
+        FullName,
+        PhoneNumber,
+        EmailAddress,
+        CompanyName,
+        Message,
+    } = req.body
+
     console.log("Received Form Data")
-    console.log(ForData)
 
+    try {
+        await transporter.sendMail({
+            from: `"Website Contact <${process.env.EMAIL_USER}>"`,
+            to: process.env.EMAIL_USER,
+            replyTo: EmailAddress,
+            subject: "New Contact Form Message",
+            html: `
+            <h2> New Message from Website</h2>
+            <p> Full Name : ${FullName} </p>
+            <p> Phone Number : ${PhoneNumber} </p>
+            <p> Email Address : ${EmailAddress} </p>
+            <p> Company Name : ${CompanyName} </p>
+            <p> Message : ${Message} </p>
+            `
+        })
 
-    res.status(200).json({
-        success: true,
-        message: "Form Data Received SuccessFully"
-    })
+        res.status(200).json({
+            success: true,
+            message: "Email Sent successfully"
+        })
+    } catch (err) {
+        console.error("Email Error : ", err)
+
+        res.status(500).json({
+            success: false,
+            message: "Failed to send email"
+        })
+    }
 })
 
 
